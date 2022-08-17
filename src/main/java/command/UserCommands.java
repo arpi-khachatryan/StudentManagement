f7e159d18982b09c350388bd1bd466c1e619fbf9
arpikhachatryan <arpikhachatryan1@gmail.com>
2022-08-17 23:24:00 +0400

    UserCommands

--- src/main/java/command/UserCommands.java
+++ src/main/java/command/UserCommands.java
@@ -8,9 +8,9 @@ public interface UserCommands extends Commands {
         System.out.println("Please input " + LOGOUT + " for logout");
         System.out.println("Please input " + ADD_STUDENT + " to add student");
         System.out.println("Please input " + PRINT_ALL_STUDENTS + " to print the student/students");
+        System.out.println("Please input " + PRINT_STUDENTS_COUNT + " to print the number of students");
         System.out.println("Please input " + DOWNLOAD_STUDENTS_EXCEL + " to download student data into an excel");
         System.out.println("Please input " + READING_FROM_EXCEL + " to read from excel");
-        System.out.println("Please input " + PRINT_STUDENTS_COUNT + " to print the number of students");
         System.out.println("Please input " + PRINT_STUDENT_BY_LESSON + " to print the student/students by lesson");
         System.out.println("Please input " + PRINT_LESSONS + " to print all lessons");
     }
